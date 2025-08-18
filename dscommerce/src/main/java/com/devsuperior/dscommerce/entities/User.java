2130c57cb25b591d841fef880d48fb362eb40b33
Allana Avila <allanavila@gmail.com>
2025-08-18 08:39:31 -0300

    criando as classes Product e Category e suas relacoes de muitos para muitos

--- dscommerce/src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -14,7 +14,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Column(unique = true)
     private String email;
+
     private String phone;
     private LocalDate birthDate;
     private String password;
@@ -24,6 +27,7 @@ public class User {
 
 
     public User(){}
+
     public User(Long id,String name, String email, String phone, LocalDate birthDate, String password) {
         this.id = id;
         this.name = name;
@@ -36,51 +40,39 @@ public class User {
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getPhone() {
         return phone;
     }
-
     public void setPhone(String phone) {
         this.phone = phone;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public LocalDate getBirthDate() {
         return birthDate;
     }
-
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
     public List<Order> getOrders() {
         return orders;
     }
