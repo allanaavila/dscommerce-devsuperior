82c0970121b143e36cb77b52d9f5d6a70108a2cc
Allana Avila <allanavila@gmail.com>
2025-08-19 13:50:29 -0300

    criando a parte de controller

--- dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -24,5 +25,10 @@ public class ProductController {
     public ProductDTO findById(@PathVariable Long id) {
         ProductDTO dto = service.findById(id);
         return dto;
+    }
+
+    @GetMapping
+    public List<ProductDTO> findAll() {
+        return service.findAll();
     }
 }
