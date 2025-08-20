a9f5eefc43e0ebf688de9aafcde0ec18fa4deef5
Allana Avila <allanavila@gmail.com>
2025-08-20 11:19:54 -0300

    criando CustomError

--- dscommerce/src/main/java/com/devsuperior/dscommerce/dto/CustomError.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/dto/CustomError.java
@@ -1,19 +1,21 @@
 package com.devsuperior.dscommerce.dto;
 
+import java.time.Instant;
+
 public class CustomError {
-    private String timestamp;
+    private Instant timestamp;
     private Integer status;
     private String error;
     private String path;
 
-    public CustomError(String timestamp, Integer status, String error, String path) {
+    public CustomError(Instant timestamp, Integer status, String error, String path) {
         this.timestamp = timestamp;
         this.status = status;
         this.error = error;
         this.path = path;
     }
 
-    public String getTimestamp() {return timestamp;}
+    public Instant getTimestamp() {return timestamp;}
     public Integer getStatus() {return status;}
     public String getError() {return error;}
     public String getPath() {return path;}
